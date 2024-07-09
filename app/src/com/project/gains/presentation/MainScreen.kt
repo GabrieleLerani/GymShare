951c3563f42443a8417f5d7a893e6d4f9ca8cb43
GabrieleLerani <gabriele.lerani2000@gmail.com>
2024-07-09 13:24:51 +0200

    Search not completed yet

--- app/src/com/project/gains/presentation/MainScreen.kt
+++ app/src/com/project/gains/presentation/MainScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 
 
 import com.project.gains.presentation.components.DynamicBottomBar
 import com.project.gains.presentation.components.DynamicTopBar
+import com.project.gains.presentation.components.SearchViewModel
 
 import com.project.gains.presentation.navgraph.NavGraph
 import com.project.gains.theme.GOrange
@@ -16,8 +18,13 @@ import com.project.gains.theme.GainsAppTheme
 
 
 @Composable
-fun MainScreen(startDestination: String) {
+fun MainScreen(startDestination: String, searchViewModel: SearchViewModel) {
     val navController = rememberNavController()
+
+    val searchWidgetState by searchViewModel.searchWidgetState
+    val searchTextState by searchViewModel.searchTextState
+
+
     GainsAppTheme {
         Scaffold(
             topBar = { DynamicTopBar(navController = navController) },
