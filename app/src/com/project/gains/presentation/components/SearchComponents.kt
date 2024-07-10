6866be9e47ff35cea8605accdfd78ccb42da53ac
Carlo <giralda.carlo@gmail.com>
2024-07-10 18:24:09 +0200

    Keyboard auto-start

--- app/src/com/project/gains/presentation/components/SearchComponents.kt
+++ app/src/com/project/gains/presentation/components/SearchComponents.kt
@@ -1,14 +1,13 @@
 package com.project.gains.presentation.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,6 +20,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,15 +28,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.project.gains.data.Categories
-import com.project.gains.theme.GainsAppTheme
 
 @Composable
 fun SearchAppBar(
@@ -51,11 +52,23 @@ fun SearchAppBar(
     enabled: Boolean
 ) {
 
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val focusRequester = FocusRequester()
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     TextField(
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onClick() }
-            .padding(15.dp),
+            .padding(15.dp)
+            .focusRequester(focusRequester)
+            .onFocusChanged {
+                if (isFocused) {
+                    keyboardController?.show()
+                }
+            },
+        interactionSource = interactionSource,
         enabled = enabled,
         value = text,
         onValueChange = {
@@ -116,9 +129,12 @@ fun SearchAppBar(
         ),
         shape = RoundedCornerShape(16.dp)
     )
+
+    DisposableEffect(Unit) {
+        focusRequester.requestFocus()
+        onDispose { }
+    }
 }
-
-
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
